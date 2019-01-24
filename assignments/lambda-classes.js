732fbd802225a8fdf3501082e52e1085589a10e5
AAsriyan <aasriyan@protonmail.com>
2019-01-24 16:12:26 -0500

    Finished the MVP for JavaScript IV. Working on Stretch tasks now.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -36,7 +36,6 @@ class Student extends Instructor {
 		this.previousBackground = studAttributes.previousBackground;
 		this.className = studAttributes.className;
 		this.favSubjects = studAttributes.favSubjects;
-		this.listsSubjects = studAttributes.listsSubjects;
 	}
 
 	listsSubjects() {
@@ -44,11 +43,11 @@ class Student extends Instructor {
 	}
 
 	PRAssignment(subject) {
-		console.log(`${student.name} has submitted a PR for ${subject}`);
+		console.log(`${this.name} has submitted a PR for ${subject}`);
 	}
 
 	sprintChallenge(subject) {
-		console.log(`${student.name} has begun sprint challenge on ${subject}`);
+		console.log(`${this.name} has begun sprint challenge on ${subject}`);
 	}
 }
 
@@ -67,3 +66,66 @@ class ProjectManagers extends Instructor {
 		console.log(`${this.name} debug's ${student.name}'s code on ${subject}`);
 	}
 }
+
+// Example of an object built off of the Instructor class
+const josh = new Instructor({
+	name: 'Josh',
+	age: 32,
+	location: 'Christmas Land',
+	gender: 'Male',
+	specialty: 'Front-end',
+	favLanguage: 'JavaScript',
+	catchPhrase: "Isnt this really cool? I think it's super cool.",
+})
+
+// Example of an object built off of the Student class
+
+const billy = new Student({
+	name: 'Billy',
+	age: 21,
+	location: 'Antarctica',
+	gender: 'Male',
+	specialty: 'Nothing',
+	favLanguage: 'JavaScript',
+	previousBackground: 'Collecting acorns for the local squirells',
+	className: 'WEB17',
+	favSubjects: ['React', 'HTML', 'CSS', 'JavaScript']
+})
+
+const jilly = new Student({
+	name: 'Jilly',
+	age: 21,
+	location: 'The North Pole',
+	gender: 'Female',
+	specialty: 'Nothing',
+	favLanguage: 'Python',
+	previousBackground: 'Worked at the Girl Scouts',
+	className: 'WEB17',
+	favSubjects: ['HTML', 'CSS', 'Redux', 'Python']
+})
+
+// Example of an object built off of the ProjectManagers class
+
+const melinda = new ProjectManagers({
+	name: 'Melinda',
+	age: 28,
+	location: 'Somewhere in Central, US',
+	gender: 'Female',
+	specialty: 'Back-end',
+	favLanguage: 'Python',
+	catchPhrase: 'Who am I missing?',
+	gradClassName: 'CS14',
+	favInstructor: 'Josh',
+})
+
+// console.log(josh.catchPhrase);
+// console.log(josh.demo('Java'));
+// console.log(josh.speak());
+// console.log(billy.listsSubjects());
+// console.log(billy.PRAssignment('JavaScript'));
+// console.log(billy.sprintChallenge('JavaScript'));
+// console.log(melinda.name);
+// console.log(melinda.favLanguage);
+// console.log(melinda.gradClassName);
+// console.log(melinda.standUp('WEB17'));
+// console.log(melinda.debugsCode(billy, 'JavaScript IV'));
